fixed the normalization of SentenceTransformer

generate_data/generate_embeddings.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
+#os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 import torch
 import torch.nn as nn
@@ -9,11 +9,6 @@ from torch.utils.data import Dataset, DataLoader
 
 import numpy as np
 import pandas as pd
-
-#from transformers import AutoTokenizer, AutoModel
-#from datasets import load_dataset, Dataset as HFDataset
-#import re
-#import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
@@ -47,7 +42,8 @@ import pickle
 with open(wiki_path, 'rb') as f:
     all_paragraphs = pickle.load(f)
 
-#all_paragraphs = all_paragraphs[:10_000] 
+
+#all_paragraphs = all_paragraphs[:1_000] 
 
 num_samples = len(all_paragraphs)
 print(f"Train set size: {len(train_indices)}")
@@ -59,38 +55,34 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = SentenceTransformer(models[m_name],
                             trust_remote_code = True
                             ).to("cuda")
-model.eval()
 
+model.eval()
 embeddings = []
 
-batch_size = 256
+batch_size = 128
 num_batches = (len(all_paragraphs) + batch_size - 1) // batch_size
 
 for i in tqdm(range(num_batches), desc="Generating Embeddings"):
-    print("the device", device) 
     start_idx = i * batch_size
     end_idx = min((i + 1) * batch_size, len(all_paragraphs))
     batch_texts = all_paragraphs[start_idx:end_idx]
 
-    #with torch.no_grad():
     batch_embeddings = model.encode(
         batch_texts,
         batch_size=batch_size,
         show_progress_bar=False,
-        normalize_embeddings=True,
+        normalize_embeddings=False,
         convert_to_numpy=True,
-        device=device
+        max_length = 512,
+        device="cuda"
     )
 
     embeddings.append(batch_embeddings)
     
     # Log GPU memory usage after each batch
-    allocated = torch.cuda.memory_allocated(device) / (1024 ** 3)
-    reserved = torch.cuda.memory_reserved(device) / (1024 ** 3)
-    print(f"Batch {i+1}/{num_batches}: Allocated GPU Memory: {allocated:.2f} GB, Reserved GPU Memory: {reserved:.2f} GB")
-
-    del batch_embeddings
-    torch.cuda.empty_cache()
+    #allocated = torch.cuda.memory_allocated(device) / (1024 ** 3)
+    #reserved = torch.cuda.memory_reserved(device) / (1024 ** 3)
+    #print(f"Batch {i+1}/{num_batches}: Allocated GPU Memory: {allocated:.2f} GB, Reserved GPU Memory: {reserved:.2f} GB")
 
 embeddings = np.vstack(embeddings)
 
@@ -98,15 +90,15 @@ print(f"Embeddings shape: {embeddings.shape}")
 
 
 # Split embeddings based on saved indices
+
 train_data = embeddings[train_indices]
 val_data = embeddings[val_indices]
 
 # Save embeddings using compressed .npz format to save space
-save_dir = f"embeddings_data/{m_name}_wiki_500k"
+save_dir = f"embeddings_data/new_{m_name}_wiki_500k"
 os.makedirs(save_dir, exist_ok=True)
 
 np.save(os.path.join(save_dir, "train_embeddings.npy"), train_data)
 np.save(os.path.join(save_dir, "val_embeddings.npy"),   val_data)
-
 print(f"Train embeddings saved to: {save_dir}/train_embeddings.npy")
 print(f"Validation embeddings saved to: {save_dir}/val_embeddings.npy")